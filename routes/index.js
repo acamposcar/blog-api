0e1e5500d7717226bdac66ec01521e154ded59ee
Alejandro Campos <acamposcar@gmail.com>
2022-05-03 23:23:35 +0200

    Add routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const indexController = require('../controllers/postController')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.post('/login', indexController.login)
+router.post('/register', indexController.register)
 
-module.exports = router;
+module.exports = router
