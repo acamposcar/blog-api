917ff38f1f75c16f91552eb7a5e1db28675ba171
Alejandro Campos <acamposcar@gmail.com>
2022-05-10 01:55:13 +0200

    Change index url to api/

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter)
+app.use('/api', indexRouter)
 app.use('/api/v1/posts', postRouter)
 app.use('/api/v1/users', usersRouter)
 
